27a59016d9ec1c828b5f105874e700e8e7f506b7
Peter <kidjinwoo@me.com>
2021-03-03 00:24:40 +0900

    iPhone & iPad Layout 분기 처리

---
+++ iPhone iPad Layout 분기 처리/CompactRegularLayout/CompactRegularLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompactRegularLayout
+//
+//  Created by Jinwoo Kim on 3/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
